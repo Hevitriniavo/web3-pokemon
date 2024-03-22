772782d5d5bcea750ab6830bca12c835453adbce
Hevitriniavo <hei.tantely@gmail.com>
2024-03-22 17:24:25 +0100

    feat: created detail pokemon

--- app/server/[id]/page.tsx
+++ app/server/[id]/page.tsx
@@ -1,10 +1,37 @@
-import { getDataById } from "@/app/services/fetch"
+import React from 'react';
+import { getDataById } from "@/app/services/fetch";
+import { Pokemon } from '@/app/types/type';
+import Image from 'next/image';
 
-export default async function PokemonDetailsSSR({params}: {params: {id: number}}) {
-    const pokemon = await getDataById(params.id)
-    console.log(pokemon)
-    return (
-      <div>page</div>
-    )
-  }
-  
+export default async function PokemonDetailsSSR({ params }: { params: { id: number } }) {
+  const pokemon: Pokemon = await getDataById(params.id);
+  console.log(pokemon);
+
+  return (
+    <div className="m-6 rounded border border-red-300">
+      <div className="flex justify-center items-center">
+        <Image
+          src={pokemon.sprites.other.showdown.front_default}
+          alt={`Image de ${pokemon.name}`}
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className='flex justify-evenly items-center'>
+        <div>
+          <h1 className="">{pokemon.name}</h1>
+          <p className=''>Expérience de base: {pokemon.base_experience}</p>
+          <p className=''>Hauteur: {pokemon.height}</p>
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold">Abilités</h2>
+          <ul className="flex flex-col space-y-1">
+            {pokemon.abilities.map((abilityInfo, index) => (
+              <li key={index} className=''>{abilityInfo.ability.name} (Slot {abilityInfo.slot}, {abilityInfo.is_hidden ? 'caché' : 'visible'})</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/services/fetch.ts
+++ app/services/fetch.ts
@@ -5,16 +5,16 @@ export async function getData(): Promise<ResponsePokemon[]>  {
     const data = await response.json();
   
     const finalData: ResponsePokemon[] = await Promise.all(data.results.map(async (pokemon: PokemonResult) => {
-      const response = await fetch(pokemon.url);
-      const pokemonDetail = await response.json();
+      const res = await fetch(pokemon.url);
+      const pokemonDetail = await res.json();
       return { name: pokemon.name, details: [pokemonDetail] };
     }));
   
     return finalData;
   };
 
-export async function getDataById(order: number): Promise<Pokemon[]>  {
+export async function getDataById(order: number): Promise<Pokemon>  {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${order}`);
-    const pokemon : Pokemon[] = await response.json();
+    const pokemon : Pokemon = await response.json();
     return pokemon;
   };
